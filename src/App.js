6c210bb9dded8c2e68c4bb70710abbaff2c2aa24
Drue Gilbert <drue.gilbert@gmail.com>
2021-03-30 18:34:40 -0700

    started adding routes

--- src/App.js
+++ src/App.js
@@ -1,6 +1,8 @@
 import React from 'react';
 import Spreadsheet from './Spreadsheet';
 import Table from './components/Table';
+import Blah from './components/Blah';
+import {BrowserRouter, Route, Link} from 'react-router-dom';
 
 export default class App extends React.Component {
 	constructor(props) {
@@ -17,10 +19,14 @@ export default class App extends React.Component {
 
 	render() {
 		return (
+			<BrowserRouter>
 			<div className="container-fluid">
 				<h2>Data Comparison Spreadsheet Tool</h2>
+				<Link to="/blah" className="btn btn-primary">blah</Link>
+				<Route exact path="/blah" component={Blah}/>
 				<Table key="Table" sheet={this.state.sheet} sheetUpdated={this.sheetUpdated}/>
 			</div>
+			</BrowserRouter>
 		);
 	}
 }
---
+++ src/components/Blah.js
@@ -0,0 +1,13 @@
+import React from 'react';
+
+export default class Blah extends React.Component {
+	constructor(props) {
+		super(props);
+	}
+
+	render() {
+		return (
+			<h1>Is this working?</h1>
+		);
+	}
+}
